2f28f2d60f678cf5d04c060acdd6e80ac068ded4
jonovu <30971561+jonovu@users.noreply.github.com>
2019-02-07 11:17:07 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
     $('#jasoncall').hover(function() {
         $('.past-logo-bg').css("background-image", "url(images/jasoncall-bg.jpg)");
     });
+    $('#kyle').hover(function() {
+        $('.past-logo-bg').css("background-image", "url(images/kyle-bg.jpg)");
+    });
+    $('#archabald').hover(function() {
+        $('.past-logo-bg').css("background-image", "url(images/archabald-bg.jpeg)");
+    });
+    $('#hiddenhos').hover(function() {
+        $('.past-logo-bg').css("background-image", "url(images/hiddenhos-bg.jpg)");
+    });
+    $('#strangevac').hover(function() {
+        $('.past-logo-bg').css("background-image", "url(images/strangevac-bg.jpg)");
+    });
+    $('#drmcthr').hover(function() {
+        $('.past-logo-bg').css("background-image", "url(images/drmcthr-bg.jpg)");
+    });
  });
