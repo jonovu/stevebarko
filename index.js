4bfe626aab6ad96ad39b0c2c48f4e7b70bd954d2
jonovu <30971561+jonovu@users.noreply.github.com>
2019-02-10 01:40:56 +1100

    fourth? change

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
+    
     $('#jasoncall').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/jasoncall-bg.jpg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/jasoncall-bg.jpg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.jasoncall-lt').show();
+        $('.past-img-credit').text(function () {
+            return "Jason Call Music";
+        });
     });
     $('#kyle').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/kyle-bg.jpg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/kyle-bg.jpg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.kyle-lt').show();
+        $('.past-img-credit').text(function () {
+            return "Kyle Castellani";
+        });
     });
     $('#archabald').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/archabald-bg.jpeg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/archabald-bg.jpeg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.archabald-lt').show();
+        $('.past-img-credit').text(function () {
+            return "Archabald";
+        });
     });
     $('#hiddenhos').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/hiddenhos-bg.jpg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/hiddenhos-bg.jpg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.hiddenhos-lt').show();
+        $('.past-img-credit').text(function () {
+            return "Hidden Hospitals";
+        });
     });
     $('#strangevac').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/strangevac-bg.jpg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/strangevac-bg.jpg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.strangevac-lt').show();
+        $('.past-img-credit').text(function () {
+            return "Strange Vacation";
+        });
     });
     $('#drmcthr').hover(function() {
-        $('.past-logo-bg').css("background-image", "url(images/drmcthr-bg.jpg)");
+        $('.past-logo-bg').hide().delay(80).css("background-image", "url(images/drmcthr-bg.jpg)").fadeIn(400);
+        $('.linktree').hide();
+        $('.drmcthr-lt').show();
+        $('.past-img-credit').text(function () {
+            return "DRMCTHR";
+        });
     });
+    
  });
